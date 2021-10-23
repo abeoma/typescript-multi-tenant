30a0450c665cb1214d7c6660e9f1064fe081aed4
abeoma <abmckz@gmail.com>
2021-10-23 15:57:35 +0900

    Fix user controller.

--- server/infra/https/routers/app/user.ts
+++ server/infra/https/routers/app/user.ts
@@ -4,13 +4,11 @@ import express from "express";
 const userRouter = express.Router();
 
 userRouter.get("/", (req, res) => {
-  const reg = req.app.get("registry");
-  new UserController(reg).getUsers(req, res);
+  new UserController().getUsers(req, res);
 });
 
 userRouter.post("/", (req, res) => {
-  const reg = req.app.get("registry");
-  new UserController(reg).createUser(req, res);
+  new UserController().createUser(req, res);
 });
 
 export { userRouter };
--- server/subdomains/users/controllers/user.ts
+++ server/subdomains/users/controllers/user.ts
@@ -1,21 +1,14 @@
 import { UserApplicationService } from "./../services/user";
 import { BaseController } from "../../../shared/infra/http/models/BaseController";
 import express from "express";
-import { IRegistry } from "../../../infra/database/interfaces/registry";
 
 export class UserController extends BaseController {
-  private reg: IRegistry;
-
-  constructor(reg: IRegistry) {
-    super();
-    this.reg = reg;
-  }
-
   async getUsers(
     req: express.Request,
     res: express.Response
   ): Promise<express.Response> {
-    const users = await new UserApplicationService(this.reg).fetchUsers();
+    const reg = req.app.get("registry");
+    const users = await new UserApplicationService(reg).fetchUsers();
     return this.ok(res, users);
   }
 
@@ -23,7 +16,8 @@ export class UserController extends BaseController {
     req: express.Request,
     res: express.Response
   ): Promise<unknown> {
-    new UserApplicationService(this.reg).registerUser({
+    const reg = req.app.get("registry");
+    new UserApplicationService(reg).registerUser({
       email: `test+${String(Math.random())}@gmail.com`,
       password: "test",
       firstName: "taro",
