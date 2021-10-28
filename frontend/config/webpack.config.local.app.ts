d2bb35f63d395cc98fa5bb225e2b459e759e1e82
abeoma <abmckz@gmail.com>
2021-10-29 08:24:14 +0900

    Update webpack.config

---
+++ frontend/config/webpack.config.local.app.ts
@@ -0,0 +1,25 @@
+import { WebpackOptionsNormalized } from "webpack";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import baseConfig from "./webpack.config";
+
+const tenantId = "barasu-dev";
+const proxy = createProxyMiddleware({
+  target: "http://localhost:5000",
+  onProxyReq: function (req, _res, _proxyOptions) {
+    req.setHeader("X-TENANT", tenantId);
+  },
+});
+
+const config: WebpackOptionsNormalized = {
+  ...baseConfig,
+  devServer: {
+    historyApiFallback: {
+      index: "/dist/index.html",
+    },
+    proxy: {
+      "/api": proxy,
+    },
+  },
+};
+
+export default config;
--- frontend/config/webpack.config.ts
+++ frontend/config/webpack.config.ts
@@ -1,39 +1,98 @@
+import { WebpackOptionsNormalized } from "webpack";
 import HtmlWebPackPlugin from "html-webpack-plugin";
 import path from "path";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
-  template: "./index.html",
+  template: path.resolve(__dirname, "../index.html"),
   filename: "./index.html",
 });
-module.exports = {
-  entry: "./index.tsx",
-  output: {
-    path: path.resolve("dist"),
-    filename: "[name].js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx$/,
-        use: {
-          loader: "ts-loader",
-        },
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ["css-loader"],
-      },
+
+const typescriptRule = {
+    test: /\.tsx?$/,
+    use: {
+      loader: "ts-loader",
+    },
+    include: [
+      path.resolve(__dirname, "../app"),
+      path.resolve(__dirname, "../lib"),
+      path.resolve(__dirname, "../../node_modules/shared"),
     ],
+    exclude: [/\.test\.tsx?$/],
+  },
+  jsRule = {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+    },
+  },
+  cssRule = {
+    test: /\.css$/,
+    use: ["css-loader"],
+    include: [path.resolve(__dirname, "node_modules/semantic-ui-css")],
+  };
+
+type Mode = "production" | "development";
+const mode: Mode = isProduction ? "production" : "development";
+
+const baseConfig = {
+  mode,
+  context: __dirname,
+  cache: !isProduction && {
+    type: "filesystem" as const,
+    buildDependencies: {
+      config: [__filename],
+    },
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          name: "vendor",
+          chunks: "initial" as const,
+          enforce: true,
+        },
+      },
+    },
+  },
+  output: {
+    filename: "[name]-[contenthash].js",
+    publicPath: "/dist/",
+    path: path.join(__dirname, process.env.DIST_PATH || "dist"),
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+    modules: ["node_modules"],
+    alias: {
+      "date-fns": "date-fns/esm",
+      lodash: "lodash-es", // Trailing $ is needed because lodash-es does not support lodash/fp.
+    },
+    symlinks: false,
+  },
+  experiments: {},
+  externals: {},
+  externalsPresets: {},
+  infrastructureLogging: {},
+  node: {},
+  resolveLoader: {},
+  snapshot: {},
+  stats: {},
+  watchOptions: {},
+};
+
+const config: WebpackOptionsNormalized = {
+  ...baseConfig,
+  entry: { main: { import: [path.join(__dirname, "../app/", "index.tsx")] } },
+  module: {
+    defaultRules: [],
+    generator: {},
+    parser: {},
+    rules: [typescriptRule, jsRule, cssRule],
   },
   plugins: [htmlWebpackPlugin],
 };
+
+export default config;
