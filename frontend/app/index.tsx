ec6e4d45041f8b9b18984f01ec26cd693fa594c1
abeoma <abmckz@gmail.com>
2021-10-30 12:07:31 +0900

    Rename shared common.

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,6 +1,6 @@
 import configureApplication from "../lib/configureApplication";
 import Root from "./containers/Root";
-import { assertIsDefined } from "shared/asserts";
+import { assertIsDefined } from "@barasu/common/asserts";
 
 const app = configureApplication();
 
