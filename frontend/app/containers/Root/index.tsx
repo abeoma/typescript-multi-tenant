5d74b7f59bd62cb8f60109b89e211199ebfd43fb
abeoma <abmckz@gmail.com>
2021-11-03 08:46:30 +0900

    Remove GlobalStyle.

--- frontend/app/containers/Root/index.tsx
+++ frontend/app/containers/Root/index.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import App from "../App";
-import GlobalStyle from "../../../lib/GlobalStyle";
 
 const Root = () => {
   return (
     <React.Fragment>
       <App />
-      <GlobalStyle />
     </React.Fragment>
   );
 };
