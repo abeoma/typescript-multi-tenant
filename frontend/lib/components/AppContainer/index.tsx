029ab28adb21ffbf37c88b5054e9e6c1718b4e35
Abe <abmckz@gmail.com>
2021-11-03 21:04:45 +0900

    Introduce Material UI. (#13)
    * Remove GlobalStyle.
    
    * Add material ui and remove styled-components.
    
    * Add @emaion package.
    
    * Add AppContainer.
    
    * Change AppBar color.
    
    * Fix sidebar width on tablet and mobile.
    
    * Fix sidebar link text style.
    
    * Fix sidebar tooltip font size.

---
+++ frontend/lib/components/AppContainer/index.tsx
@@ -0,0 +1,166 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  AppBarProps,
+  Badge,
+  Box,
+  Container,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  fontColor,
+  headerColorBg,
+  mediaTabletAndMobile,
+} from "../../style-variables";
+
+const DRAWER_WIDTH = 240;
+
+const StyledAppBar = styled(AppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps & { open?: boolean }>(({ theme, open }) => ({
+  background: headerColorBg,
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: DRAWER_WIDTH,
+    width: `calc(100% - ${DRAWER_WIDTH}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const StyledDrawer = styled(Drawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: DRAWER_WIDTH,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [mediaTabletAndMobile]: {
+        width: 0,
+      },
+    }),
+  },
+}));
+
+type Props = { children: React.ReactNode; leftNav?: React.ReactNode };
+
+const AppContainer = ({ children, leftNav }: Props) => {
+  const [open, setOpen] = useState(false);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <StyledAppBar position="absolute" open={open}>
+        <Toolbar
+          sx={{
+            pr: "24px", // keep right padding when drawer closed
+          }}
+        >
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer}
+            sx={{
+              marginRight: "36px",
+              display: "none",
+              [mediaTabletAndMobile]: {
+                display: open ? "none" : "block",
+              },
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            Barasu
+          </Typography>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </StyledAppBar>
+      <StyledDrawer
+        variant="permanent"
+        open={open}
+        sx={{
+          ["& a"]: {
+            textDecoration: "none",
+            color: fontColor,
+          },
+        }}
+      >
+        <Toolbar
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            px: [1],
+          }}
+        >
+          <IconButton onClick={toggleDrawer}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Toolbar>
+        <Divider />
+        {leftNav}
+      </StyledDrawer>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: "100vh",
+          overflow: "auto",
+        }}
+      >
+        <Toolbar />
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          {children}
+        </Container>
+      </Box>
+    </Box>
+  );
+};
+
+export default AppContainer;
--- frontend/lib/style-variables.ts
+++ frontend/lib/style-variables.ts
@@ -1,1 +1,8 @@
 export const baseFontSize = "16px";
+export const smallFontSize = "0.85714286rem";
+
+export const fontColor = "rgba(0,0,0,0.87)";
+export const headerColorBg = "#253342";
+
+const tabletMaxWidth = 1024;
+export const mediaTabletAndMobile = `@media screen and (max-width: ${tabletMaxWidth}px)`;
