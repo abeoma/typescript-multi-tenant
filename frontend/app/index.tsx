56297cf4fd92bc63ab6ce058abad3bd9e3b9090c
abeoma <abmckz@gmail.com>
2021-11-06 16:03:53 +0900

    Update reducers.

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,8 +2,12 @@ import configureApplication from "../lib/configureApplication";
 import Root from "./containers/Root";
 import { assertIsDefined } from "@barasu/common/asserts";
 import rootSaga from "./sagas";
+import { viewState, entities } from "./reducers";
 
-const app = configureApplication(rootSaga);
+const app = configureApplication({
+  reducers: { viewState, entities },
+  saga: rootSaga,
+});
 
 const target = document.getElementById("index");
 assertIsDefined(target);
--- frontend/app/reducers/index.ts
+++ frontend/app/reducers/index.ts
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
 import usersReducer from "../containers/UsersPage/reducer";
+import { entities } from "./entities";
 
 export const viewState = combineReducers({
   users: usersReducer,
 });
+
+export { entities };
