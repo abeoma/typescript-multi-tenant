5a2cbbf60b3557e063154d8f8a6208f592bb5db8
abeoma <abmckz@gmail.com>
2021-11-11 06:43:00 +0900

    Fix sagas/api.

--- frontend/app/sagas/api.ts
+++ frontend/app/sagas/api.ts
@@ -1,12 +1,25 @@
 import { call } from "@redux-saga/core/effects";
 import { fetch } from "cross-fetch";
 import endpoint from "./endpoint";
+import { User } from "../schema";
+import { SagaIterator } from "@redux-saga/types";
 
-async function requestInternalRaw(args: { endpoint: string }) {
-  const res = await fetch(args.endpoint, { method: "GET" });
-  return res.text();
+type Method = "GET" | "POST" | "PUT" | "DELETE";
+type RawResponse = Record<string, unknown>;
+
+async function requestRaw(
+  endpoint: string,
+  method: Method
+): Promise<RawResponse> {
+  const res = await fetch(endpoint, { method });
+  return res.json();
 }
 
-export function* loadUserEntities(): Generator {
-  return yield call(requestInternalRaw, { endpoint: endpoint.users });
+function* requestGet(endpoint: string): SagaIterator {
+  const resJson: RawResponse = yield call(requestRaw, endpoint, "GET");
+  return resJson;
+}
+
+export function* loadUserEntities(): SagaIterator<User[]> {
+  return yield call(requestGet, endpoint.users);
 }
