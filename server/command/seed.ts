5d40e83955b63a1f9bc7fa786a2722890876f8fe
abeoma <abmckz@gmail.com>
2021-11-13 20:26:40 +0900

    Fix seed.

--- server/command/seed.ts
+++ server/command/seed.ts
@@ -1,6 +1,6 @@
 import { UserModel } from "../infra/database/typeorm/models/tenant/user";
 import { Connection } from "typeorm";
-import { range } from "../lib/helpers";
+import { choice, range } from "../lib/helpers";
 import { v4 as uuidv4 } from "uuid";
 import faker from "faker";
 
@@ -16,23 +16,23 @@ export class SeedImporter {
   }
 
   private async importUsers() {
-    const users = range(50).map((_) => ({
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-    }));
+    const users = range(50).map((_) => {
+      const firstName = faker.name.firstName();
+      const lastName = faker.name.lastName();
+      return {
+        id: uuidv4(),
+        email: `${firstName}.${lastName}@example.com`,
+        password: "passwOrd",
+        firstName,
+        lastName,
+        isActive: choice([true, true, true, false]),
+      };
+    });
     await this.conn
       .createQueryBuilder()
       .insert()
       .into(UserModel)
-      .values(
-        users.map((u) => ({
-          id: uuidv4(),
-          email: `${u.firstName}.${u.lastName}@example.com`,
-          password: "password",
-          isActive: true,
-          ...u,
-        }))
-      )
+      .values(users)
       .execute();
   }
 }
