362b2f885bbf88b8d5ac208cad83b9cde131b672
abeoma <abmckz@gmail.com>
2021-11-21 15:45:09 +0900

    Create user from UsersPage.

--- frontend/app/containers/UsersPage/slice.ts
+++ frontend/app/containers/UsersPage/slice.ts
@@ -1,21 +1,40 @@
 import {
   createEntityAdapter,
   createSlice,
+  EntityState,
   PayloadAction,
 } from "@reduxjs/toolkit";
 import { User } from "../../schema";
 import { RootState } from "../../store";
 
+interface UsersPageState extends EntityState<User> {
+  page: number;
+  sum: number;
+  offset: number;
+  openUserModal: boolean;
+}
+
 const usersAdapter = createEntityAdapter<User>();
 
-const initialState = usersAdapter.getInitialState({ values: { users: [] } });
+const initialState: UsersPageState = usersAdapter.getInitialState({
+  page: 1,
+  sum: 0,
+  offset: 0,
+  openUserModal: false,
+});
 
 const slice = createSlice({
   name: "users",
   initialState,
   reducers: {
-    load(state, action: PayloadAction<User[]>) {
-      usersAdapter.setAll(state, action.payload);
+    load(state, action: PayloadAction<{ users: User[] }>) {
+      usersAdapter.setAll(state, action.payload.users);
+    },
+    openUserModal(state) {
+      state.openUserModal = true;
+    },
+    closeUserModal(state) {
+      state.openUserModal = false;
     },
   },
 });
