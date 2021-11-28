f0bb7a22c200c3699e37dc2d1aa72c10fbd01633
abeoma <abmckz@gmail.com>
2021-11-28 18:44:39 +0900

    Fix common/asserts.

--- common/asserts.ts
+++ common/asserts.ts
@@ -5,26 +5,31 @@ class AssertionError extends Error {
   }
 }
 
-export const assertNever = (_: never): void => {
+type AssertNever = (val: unknown) => asserts val is never;
+export const assertNever: AssertNever = (_) => {
   // Do nothing
 };
 
-export const assertIsDefined = <T>(val: T): asserts val is NonNullable<T> => {
+type AssertIsDefined = <T>(val: T) => asserts val is NonNullable<T>;
+export const assertIsDefined: AssertIsDefined = (val) => {
   if (typeof val === "undefined" || val === null) {
     throw new Error();
   }
 };
 
-export const assertIsString = (val: unknown): asserts val is string => {
+type AssertIsString = (val: unknown) => asserts val is string;
+export const assertIsString: AssertIsString = (val) => {
   if (typeof val !== "string") throw new AssertionError("Not a string.");
 };
 
-export const assertIsBoolean = (val: unknown): asserts val is boolean => {
+type AssertIsBoolean = (val: unknown) => asserts val is boolean;
+export const assertIsBoolean: AssertIsBoolean = (val) => {
   if (typeof val !== "boolean") throw new AssertionError("Not a boolean.");
 };
 
-export const assertIsObject = (
+type AssertIsObject = (
   val: unknown
-): asserts val is { [key: string]: unknown } => {
+) => asserts val is { [key: string]: unknown };
+export const assertIsObject: AssertIsObject = (val) => {
   if (typeof val !== "object") throw new AssertionError("Not a object.");
 };
